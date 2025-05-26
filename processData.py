534c873499ec40aeeb3b470740dd0cc7e80dfef5
Hunter Wilde <hunter.wilde@connect.qut.edu.au>
2025-05-26 21:35:24 +1000

    Implemented loop to process an entire dataset of images into videos.

--- processData.py
+++ processData.py
@@ -4,18 +4,17 @@ import cv2
 got10k_path = "raw_data/GOT-10k/test/"
 
 # Generate video from a series of images
-def generate_video(image_folder, video_name="mygeneratedvideo.avi"):
-    video_name = "processed_data/" + video_name
-
+def generate_video(image_folder, video_name):
     images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".jpeg", ".png"))]
-    print("Images:", images)
+    #print("Images:", images)
 
     # Set frame from the first image
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
 
     # Video writer to create .avi file
-    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'DIVX'), 1, (width, height))
+    video = cv2.VideoWriter(f"processed_data/{video_name}.avi",
+                            cv2.VideoWriter_fourcc(*'DIVX'), 1, (width, height))
 
     # Appending images to video
     for image in images:
@@ -24,7 +23,16 @@ def generate_video(image_folder, video_name="mygeneratedvideo.avi"):
     # Release the video file
     video.release()
     cv2.destroyAllWindows()
-    print("Video generated successfully!")
+    print(f"{video_name} generated successfully!")
 
-# Calling the function to generate the video
-generate_video(got10k_path + "GOT-10k_Test_000001", "GOT-10k_Test_000001.avi")
+# Process all raw data into videos
+def process_data(raw_data_path):
+    try:
+        with open(raw_data_path + "list.txt", 'r') as file:
+            for line in file:
+                generate_video(raw_data_path + line.strip(), line.strip())
+        print(f"All videos in {raw_data_path} generated successfully!")
+    except Exception as e:
+        print(f"Error reading list.txt: {e}")
+
+process_data(got10k_path)
